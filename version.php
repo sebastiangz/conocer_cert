795b0f3cab0455f76aad1afecaa99e71ea9c7b85
Sebastian Gonzalez Zepeda <sebastiangz@hotmail.com>
2025-03-03 19:12:45 -0600

    Add files via upload

---
+++ version.php
@@ -0,0 +1,12 @@
+<?php
+// Archivo: local/conocer_cert/version.php
+// 2025 Sebastian Gonzalez Zepeda sgonzalez@infraestructuragis.com
+// Información de versión para el plugin de Certificaciones CONOCER
+
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->version   = 2025030301;        // Versión en formato YYYYMMDDXX
+$plugin->requires  = 2023100900;        // Requiere Moodle 4.3.0+
+$plugin->component = 'local_conocer_cert'; // Nombre completo del plugin
+$plugin->maturity  = MATURITY_ALPHA;    // Estado de madurez
+$plugin->release   = '1.0.0 (Alpha)';   // Versión legible por humanos
